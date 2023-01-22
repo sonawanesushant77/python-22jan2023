f7d3f13a3a6c7f90743a92723599399aca411df8
sonawanesushant77 <123285585+sonawanesushant77@users.noreply.github.com>
2023-01-22 10:25:31 +0530

    Add files via upload

---
+++ Sushant First trial file22jan2023.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hellow world")
+
+
+# In[2]:
+
+
+print ("Hellow")
+
+
+# In[3]:
+
+
+print("sushant")
+
+
+# ### Basicsof python
+
+# #### Basics of python
+
+# ##### Basics of python
+
+# ### Basics of pythoon
+
+# Variables
+
+# ### varibles
+
+# In[7]:
+
+
+x=10
+x
+
+
+# In[8]:
+
+
+print(x)
+
+
+# In[9]:
+
+
+y=100
+y
+
+
+# In[10]:
+
+
+name="sushant"
+gender="male"
+experience=15
+
+
+# In[12]:
+
+
+print=(name)
+print(gender)
+print(experience)
+
+
+# In[14]:
+
+
+Gender
+
+
+# In[15]:
+
+
+gender
+
+
+# In[16]:
+
+
+y=16
+x="sushsnt"
+x
+y
+
+
+# In[17]:
+
+
+x,y
+
+
+# In[18]:
+
+
+y,x
+
+
+# In[19]:
+
+
+print(x,y)
+
+
+# In[20]:
+
+
+z=y
+z
+
+
+# In[21]:
+
+
+y
+
+
+# In[22]:
+
+
+x=y=z="test"
+print(z)
+
+
+# In[24]:
+
+
+y
+
+
+# In[ ]:
+
+
+z
+
+
+# ### Rules for namingthe variabes
+# ### Varibles can have alphabets[A-Z,a-z]
+# ### Variable name should not start with number
+# ### Varibles do not allow spl characters other than underscore
+# ### case sensitive
+# 
+
+# In[ ]:
+
+
+Books=["DS with Python","BA with Python"]
+Books
+
+
+# In[26]:
+
+
+books
+
+
+# In[27]:
+
+
+4books=["DS with Python","BA with Python"]
+
+
+# In[35]:
+
+
+b6ooks=["DS with Python","BA with Python"]
+
+
+# In[36]:
+
+
+b6ooks
+
+
+# In[37]:
+
+
+books*lists=["DS with Python","BA with Python"]
+
+
+# In[38]:
+
+
+books_lists=["DS with Python","BA with Python"]
+
+
+# In[ ]:
+
+
+
+
