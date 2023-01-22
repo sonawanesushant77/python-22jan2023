b2cb3f8e2e235978ff21c8fea64ad247ff931db2
sonawanesushant77 <123285585+sonawanesushant77@users.noreply.github.com>
2023-01-22 13:02:58 +0530

    Add files via upload

---
+++ Pandas _sushant 22Jan2023.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import pandas as pd
+
+
+# In[6]:
+
+
+df=pd.read_csv("Bank_Personal_Loan_Modelling.csv")
+
+
+# In[7]:
+
+
+df.shape
+
+
+# In[10]:
+
+
+df.head(10)
+
+
+# In[11]:
+
+
+df.tail()
+
+
+# In[12]:
+
+
+df.tail(10)
+
+
+# In[13]:
+
+
+df.sample()
+
+
+# In[14]:
+
+
+df.sample()
+
+
+# In[15]:
+
+
+df.sample()
+
+
+# In[16]:
+
+
+df.sample()
+
+
+# In[17]:
+
+
+df.sample(10)
+
+
+# In[18]:
+
+
+df.sample(10)
+
+
+# In[19]:
+
+
+df.sample(10)
+
+
+# In[20]:
+
+
+df.dtypes
+
+
+# In[21]:
+
+
+df.info
+
+
+# In[22]:
+
+
+df.isnull().sum()
+
+
+# In[23]:
+
+
+df.describe()
+
+
+# In[24]:
+
+
+df=pd.read_csv(hotel_bookings.csv.zmdownload)
+
+
+# In[25]:
+
+
+df=pd.read_csv(hotel_bookings.csv.zmdownload)
+
+
+# In[26]:
+
+
+df.shape
+
+
+# In[27]:
+
+
+df=pd.read_csv("hotel_bookings.csv.zmdownload")
+
+
+# In[28]:
+
+
+df.shape
+
+
+# In[29]:
+
+
+df=pd.read_csv("hotel_bookings.csv.zmdownload")
+
+
+# In[30]:
+
+
+df.shape
+
+
+# In[31]:
+
+
+df.isnull().sum()
+
+
+# In[32]:
+
+
+df.describe()
+
+
+# In[33]:
+
+
+df.describe(include="all")
+
+
+# In[ ]:
+
+
+
+
